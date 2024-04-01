aeb13c2a6da3ce29dfffddb3843fe5e347535312
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-04-01 18:50:20 -0500

    update

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': 'Tech1234',
-        'HOST': 'database1.cewvxnnnahzx.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'blog-rds.c1wgcmskqb38.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
@@ -144,7 +144,7 @@ LOGIN_REDIRECT_URL = "blog:list"
 LOGIN_URL = "login"
 
 
-AWS_STORAGE_BUCKET_NAME = 'blog-s3-techtorial' # please enter your s3 bucket name
+AWS_STORAGE_BUCKET_NAME = 'blogs3paulchangblog' # please enter your s3 bucket name
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_REGION_NAME = "us-east-1" # please enter your s3 region 
 AWS_DEFAULT_ACL = 'public-read'
