560d5d0743a847467770678fe04bee43d2bb7d8f
pauldchang <153890789+pauldchang@users.noreply.github.com>
2024-04-02 19:14:08 -0500

    Update settings.py

--- src/cblog/settings.py
+++ src/cblog/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'database1', # database name in RDS is written here
         'USER': 'admin', # database master username in RDS is written here
-        'PASSWORD': 'Password1234',
-        'HOST': 'database1.c1wgcmskqb38.us-east-1.rds.amazonaws.com',  # database endpoint is written here
+        'PASSWORD': 'admin12345',
+        'HOST': 'blog-application-db.c1wgcmskqb38.us-east-1.rds.amazonaws.com',  # database endpoint is written here
         'PORT': '3306' # database port is written here
     }
 }
